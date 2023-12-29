8cd0c9bfdf158b47a1d79533ec4c5ae3a2b20e33
abderrazzak ghrawi <a.razzak2002@gmail.com>
2023-12-29 12:05:44 +0100

    last version

--- react/src/views/AdminViews/dashboardAdmin.jsx
+++ react/src/views/AdminViews/dashboardAdmin.jsx
@@ -1,21 +1,8 @@
-// Exemple de gestion des autorisations dans un composant
-import { Navigate } from "react-router-dom";
-import { useStateContext } from '../../contexts/Context.jsx';
-
-const DashboardDirecteur  = () => {
-    const { user } = useStateContext();
-
-    // Vérifier si l'utilisateur est une secrétaire
-    if (user && user.role === 'directeur') {
-        return (
-            <div>
-                {}
-            </div>
-        );
-    } else {
-        // Rediriger ou afficher un message d'erreur si l'utilisateur n'est pas autorisé
-        return <Navigate to="/unauthorized" />;
-    }
-};
-
-export default DashboardDirecteur ;
+export default function Document() {
+    return (
+        <div>
+            <h2>New Link Page</h2>
+            {}
+        </div>
+    );
+}

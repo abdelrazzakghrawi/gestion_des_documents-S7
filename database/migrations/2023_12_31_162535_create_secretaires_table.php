349994d09b7d5e5106319ae8f8cdc9525d477fe5
tahaelkhayyam1 <tahaelkhayyam1@gmail.com>
2024-01-06 17:19:06 +0100

    migrations

--- database/migrations/2023_12_31_162535_create_secretaires_table.php
+++ database/migrations/2023_12_31_162535_create_secretaires_table.php
@@ -27,5 +27,11 @@ return new class extends Migration
         });
     }
 
-
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('secretaires');
+    }
 };

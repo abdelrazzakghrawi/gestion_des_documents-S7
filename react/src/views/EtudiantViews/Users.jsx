2d9208c0384d2d2441b2b6eafa0c0f2ece0196e4
abderrazzak ghrawi <a.razzak2002@gmail.com>
2024-01-06 02:25:26 +0000

    last version le 6/1/21

--- react/src/views/EtudiantViews/Users.jsx
+++ react/src/views/EtudiantViews/Users.jsx
@@ -1,7 +1,27 @@
-export default function Users(){
-    return(
-        <div>
-            Users
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { useStateContext } from "../../contexts/Context";
+
+const Users = () => {
+    const { user } = useStateContext();
+
+    return (
+        <div style={{ margin: '20px' }}>
+            <h1>Informations de leleve</h1>
+            {user ? (
+                <Card variant="outlined">
+                    <CardContent>
+                        <Typography color="textSecondary">Nom : {user.name}</Typography>
+                        <Typography color="textSecondary">Email : {user.email}</Typography>
+                        <Typography color="textSecondary">Rôle : {user.role}</Typography>
+                    </CardContent>
+                </Card>
+            ) : (
+                <Typography color="error">Aucun utilisateur connecté</Typography>
+            )}
         </div>
-    )
-}
+    );
+};
+
+export default Users;
